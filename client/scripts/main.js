30670152f69ceb47433fc4f93ce1b062a80e12a9
Bobby <bobby@fiery.me>
2022-05-10 15:37:54 +0700

    feat: dynamic document title
    this do rely on JS, so it's mainly for aesthetics

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -7,9 +7,23 @@ function main() {
     AssetItem.define()
     AssetList.define()
 
+    let title = `${window.location.hostname} | superbindex`
+
     if (window.location.pathname === '/') {
         document.querySelector('#parent').style.display = 'none'
+    } else {
+        try {
+            const dirnames = decodeURIComponent(window.location.pathname)
+                .replace(/(^\/|\/$)/g, '')
+                .split('/')
+
+            title = `${dirnames[dirnames.length - 1]} - ${title}`
+        } catch (error) {
+            console.error(error)
+        }
     }
+
+    document.title = title
 }
 
 document.addEventListener('DOMContentLoaded', main)
